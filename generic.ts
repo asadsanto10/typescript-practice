cdd38408c1b8673a80b05cb1fe113f120e4109ce
azssrio <asaduzzamansanto@springrain.io>
2023-05-18 11:44:26 +0600

    generic

--- generic.ts
+++ generic.ts
@@ -31,8 +31,66 @@ const avs = pickObjectKey(objc, ["name", "extensions"]);
 
 // Using Generics with Functions
 
-function identity<T>(value: T): T {
+// function identity<T>(value: T): T {
+//   return value;
+// }
+
+const identity = <T>(value: T): T => {
   return value;
+};
+// console.log(identity<number>(45));
+// console.log(identity<{ name: string }>({ name: "asad" }));
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+// Passing Type Parameters Directly
+
+// async function fetchApi<ResultType>(): Promise<ResultType> {
+//   const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+//   return response.json();
+// }
+
+// (async () => {
+//   const callApi = await fetchApi<Todo[]>();
+//   console.log(callApi?.userId);
+// })();
+
+// Default Type Parameters
+async function fetchApi<
+  ResultType = Record<string, any>
+>(): Promise<ResultType> {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+  return response.json();
 }
-console.log(identity<number>(45));
-console.log(identity<string>("asd"));
+
+// (async () => {
+//   const callApi = await fetchApi();
+//   console.log(callApi.userId);
+// })();
+
+// Type Parameters Constraints
+
+const stringifyObjectKeyValues = <T extends Record<string, any>>(obj: T) => {
+  return Object.keys(obj).reduce(
+    (acc, key) => ({
+      ...acc,
+      [key]: JSON.stringify(obj[key]),
+    }),
+    {} as { [k in keyof T]: string }
+  );
+};
+
+const stringifiedValues = stringifyObjectKeyValues({
+  a: "1",
+  b: 2,
+  c: true,
+  d: [1, 2, 3],
+});
+
+// console.log(stringifiedValues);
+
+// Using Generics with Interfaces, Classes, and Types
